25916d86e6c3c272c4424599dbcf15f943b44620
khanhtran1320 <100772872+khanhtran1320@users.noreply.github.com>
2022-04-05 15:09:17 +0700

    Add files via upload

---
+++ btkt1/java/khanhtran/btkt1/MainActivity.java
@@ -0,0 +1,111 @@
+package khanhtran.btkt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* Define the UI elements */
+    private EditText eName;
+    private EditText ePassword;
+    private TextView eAttemptsInfo;
+    private Button eLogin;
+    private int counter = 5;
+
+    String userName = "";
+    String userPassword = "";
+
+    /* Class to hold credentials */
+    class Credentials
+    {
+        String name = "user";
+        String password = "123";
+    }
+
+    boolean isValid = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Bind the XML views to Java Code Elements */
+        eName = findViewById(R.id.edu);
+        ePassword = findViewById(R.id.edp);
+        eAttemptsInfo = findViewById(R.id.txt);
+        eLogin = findViewById(R.id.btnL);
+
+        /* Describe the logic when the login button is clicked */
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /* Obtain user inputs */
+                userName = eName.getText().toString();
+                userPassword = ePassword.getText().toString();
+
+                /* Check if the user inputs are empty */
+                if(userName.isEmpty() || userPassword.isEmpty())
+                {
+                    /* Display a message toast to user to enter the details */
+                    Toast.makeText(MainActivity.this, "Please enter name and password!", Toast.LENGTH_LONG).show();
+
+                }else {
+
+                    /* Validate the user inputs */
+                    isValid = validate(userName, userPassword);
+
+                    /* Validate the user inputs */
+
+                    /* If not valid */
+                    if (!isValid) {
+
+                        /* Decrement the counter */
+                        counter--;
+
+                        /* Show the attempts remaining */
+                        eAttemptsInfo.setText("Attempts: " + String.valueOf(counter));
+
+                        /* Disable the login button if there are no attempts left */
+                        if (counter == 0) {
+                            eLogin.setEnabled(false);
+                            Toast.makeText(MainActivity.this, "You have used all your attempts try again later!", Toast.LENGTH_LONG).show();
+                        }
+                        /* Display error message */
+                        else {
+                            Toast.makeText(MainActivity.this, "User and Password is wrong, please try again!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    /* If valid */
+                    else {
+
+                        Toast.makeText(MainActivity.this, "Connecting...", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        });
+    }
+
+    /* Validate the credentials */
+    private boolean validate(String userName, String userPassword)
+    {
+        /* Get the object of Credentials class */
+        Credentials credentials = new Credentials();
+
+        /* Check the credentials */
+        if(userName.equals(credentials.name) && userPassword.equals(credentials.password))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
